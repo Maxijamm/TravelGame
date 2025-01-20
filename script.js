271ec718205c3741db544a00b7e3b2bf94185a0a
Maxi May <40724892+Maxijamm@users.noreply.github.com>
2025-01-20 10:25:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,5 +31,44 @@ function toggleCircle() {
     }
 }
 
+// Function to geocode an address and update the map view
+function geocodeAddress(address) {
+    var geocodeUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
+    
+    fetch(geocodeUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data[0]) {
+                // Get the coordinates from the first result
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+                
+                // Set the view to the new coordinates
+                map.setView([lat, lon], 13);
+                
+                // Update the marker position
+                marker.setLatLng([lat, lon]).addTo(map)
+                      .bindPopup(`<b>${address}</b><br>Latitude: ${lat}, Longitude: ${lon}`)
+                      .openPopup();
+            } else {
+                alert('Address not found!');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Unable to geocode the address.');
+        });
+}
+
+// Event listener for the "Go to Address" button click
+document.getElementById('go-to-address-btn').addEventListener('click', function () {
+    var address = document.getElementById('address-input').value;
+    if (address) {
+        geocodeAddress(address);
+    } else {
+        alert('Please enter an address.');
+    }
+});
+
 // Event listener for the button click
 document.getElementById('toggle-circle-btn').addEventListener('click', toggleCircle);
